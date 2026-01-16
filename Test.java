9477e8ba9c92433a26f51c37b074ca10fadaa994
BELLAFKIH.22128 <bellafkih.deyae.studente@itispaleocapa.it>
2026-01-16 13:52:06 +0100

    Lista

---
+++ Test.java
@@ -0,0 +1,20 @@
+public class Test {
+    public static void main(String[] args) {
+        Lista lista = new Lista();
+
+        lista.inserimento("A", 0);
+        lista.inserimento("B", 1);
+        lista.inserimento("C", 1);
+
+        lista.accoda(3);
+
+        lista.eliminazione(1);
+
+        Nodo current = lista.getHead();
+        while (current != null) {
+            System.out.println(current.getValue());
+            current = current.getNext();
+        }
+    }
+
+}
